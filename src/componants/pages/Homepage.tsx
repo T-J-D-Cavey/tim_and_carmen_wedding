3fc586b8a524c0a37e85f543ecd3f33594bfa627
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-10-08 00:35:10 +0100

    choose edit crop and test photos

--- src/componants/pages/Homepage.tsx
+++ src/componants/pages/Homepage.tsx
@@ -8,6 +8,16 @@ export function Homepage() {
         <>
             <Change_language />
             <Wedding_bells />
+            <div className='main-image i1'></div>
+            <div className='main-image i2'></div>
+            <div className='main-image i3'></div>
+            <div className='main-image i4'></div>
+            <div className='main-image i5'></div>
+            <div className='main-image i6'></div>
+            <div className='main-image i7'></div>
+            <div className='main-image i8'></div>
+            <div className='main-image i9'></div>
+            <div className='main-image i10'></div>
             <p>{t("homepage")}</p>
         </>
     )
