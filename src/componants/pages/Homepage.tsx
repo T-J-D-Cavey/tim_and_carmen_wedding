4b0858519d07c74e0697267dac12b3e6c0b17051
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-10-12 18:05:38 +0100

    improve and refactor styles

--- src/componants/pages/Homepage.tsx
+++ src/componants/pages/Homepage.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { throttle } from 'lodash';
 import { Change_language } from "../widgets/Change_language";
 import audio_file from '../../../public/wedding_bells_audio.wav';
@@ -10,6 +10,7 @@ export function Homepage() {
     const hero_element = useRef<HTMLDivElement | null>(null);
     const celebrate_element = useRef<HTMLDivElement | null>(null);
     const audioRef = useRef<HTMLAudioElement | null>(null);
+    const rsvp_element = useRef<HTMLDivElement | null>(null);
     let play_count = 0;
     const throttle_scroll_events = throttle(handle_scroll_effects, 333)
 
@@ -28,9 +29,11 @@ export function Homepage() {
         }
         if(window.innerHeight - celebrate_element_coords.bottom > 0) {
             celebrate_element.current!.classList.add('appear');
+            rsvp_element.current!.classList.add('appear');
         }
         else {
             celebrate_element.current!.classList.remove('appear');
+            rsvp_element.current!.classList.remove('appear');
         }
     }
 
@@ -61,38 +64,47 @@ export function Homepage() {
             </div>
             {/* Could add a tsparticles confetti animation behind this hope_container, starting on specific scroll position */}
             <div className="section hope_container">
-                <div ref={celebrate_element} className="box">
-                    <p className='font_written'>{t("homepage_celebrate")}</p>
-                    {/* Add wedding dates here? */}
-                </div>
-                <div className="box">
-                    <div>
-                        <p className='font_written'>{t("homepage_hope")}</p>
+                <div className="box square">
+                    <div ref={celebrate_element} className='hope_appear'>
+                        <p className='font_written'>{t("homepage_celebrate")}</p>
+                        <div>
+                            <p className='font_main small_font'>{t("homepage__malay_date")}</p>
+                            <p className='font_main small_font'>{t("homepage__uk_date")}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className='font_main'>
-                            <a 
-                                href="https://docs.google.com/forms/d/e/1FAIpQLSeHz_Ge78j_BSThaY3NEYHd7BEi1B8QtIjHvVNSlxrSCLrCBA/viewform?usp=sf_link"
-                                target="_blank">
-                                {t("homepage_rsvp")}
-                            </a>
-                        </p>    
+                </div>
+                <div className="box square">
+                    <div ref={rsvp_element} className="hope_appear">
+                        <div>
+                            <p className='font_written'>{t("homepage_hope")}</p>
+                        </div>
+                        <div>
+                            <p className='font_main '>
+                                <a 
+                                    href="https://docs.google.com/forms/d/e/1FAIpQLSeHz_Ge78j_BSThaY3NEYHd7BEi1B8QtIjHvVNSlxrSCLrCBA/viewform?usp=sf_link"
+                                    target="_blank">
+                                    {t("homepage_rsvp")}
+                                </a>
+                            </p>    
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="section paypal_section_container">
-                <div className="box">
+                <div className="box square">
                     <p>{t("homepage_paypal_message")}</p>
+                    <div className="box paypal_box">
+                        <form action="https://www.paypal.com/donate" method="post" target="_blank">
+                            <input type="hidden" name="business" value="E8GXZZ6J2WQUS" />
+                            <input type="hidden" name="no_recurring" value="1" />
+                            <input type="hidden" name="item_name" value={t("homepage_paypal_page_note")} />
+                            <input type="hidden" name="currency_code" value="GBP" />
+                            <input type="image" src="https://pics.paypal.com/00/s/NmJhZDljMDctNjcxMy00NTY5LWI4Y2EtNGM4ODk0NDcxZjEw/file.PNG" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                            <img alt="" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
+                        </form>
+                    </div>
                 </div>
-                <div className="box paypal_box">
-                    <form action="https://www.paypal.com/donate" method="post" target="_blank">
-                        <input type="hidden" name="business" value="E8GXZZ6J2WQUS" />
-                        <input type="hidden" name="no_recurring" value="1" />
-                        <input type="hidden" name="item_name" value={t("homepage_paypal_page_note")} />
-                        <input type="hidden" name="currency_code" value="GBP" />
-                        <input type="image" src="https://pics.paypal.com/00/s/NmJhZDljMDctNjcxMy00NTY5LWI4Y2EtNGM4ODk0NDcxZjEw/file.PNG" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                        <img alt="" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
-                    </form>
+                <div className="box square">
                 </div>
             </div>
         </div>
