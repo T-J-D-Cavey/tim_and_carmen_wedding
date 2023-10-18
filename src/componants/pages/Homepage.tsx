6eab48cd2aeab61f9c297d0212c07ea03e83b121
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-10-18 18:50:22 +0100

    final commit before merge

--- src/componants/pages/Homepage.tsx
+++ src/componants/pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { useEffect, useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Change_language } from "../widgets/Change_language";
 import { UK_modal } from "../widgets/modals/UK_modal";
 import { Malaysia_modal } from "../widgets/modals/Malaysia_modal";
@@ -21,7 +21,7 @@ export function Homepage() {
         }
         timeout_id = setTimeout(() => {
             lang.current!.classList.remove('move_off_screen')
-        }, 2000)
+        }, 1500)
 
         if(!hero.current!.classList.contains("hero_effects") && latest > 500) return;
 
@@ -36,12 +36,9 @@ export function Homepage() {
             hero.current!.classList.remove("hero_effects");
         }
     })
-    useEffect(() => {
-    
 
-        return () => {
-            // need to work out how to clean up uesMotionValueEvent
-        }
+    useEffect(() => {
+        lang.current!.classList.add('move_off_screen');
     }, [])
 
     return (
@@ -66,44 +63,56 @@ export function Homepage() {
             <div className="section hope_container font_dark">
                 <div>
                     <div>
-                        <p className='font_written mb_1'>{t("homepage_celebrate")}</p>
-                        <div className="v_small_font font_extra_spacing">
-                            <p>{t("homepage__uk_date")}</p>
-                            <p className="mt_1">{t("homepage__malay_date")}</p>
-                        </div>
+                        <motion.div 
+                        initial={{ opacity: 0, y: -30, scale: 0.5 }}
+                        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ duration: 0.9 }} 
+                        >
+                            <p className='font_written mb_1'>{t("homepage_celebrate")}</p>
+                            <div className="v_small_font font_extra_spacing">
+                                <p>{t("homepage__uk_date")}</p>
+                                <p className="mt_1">{t("homepage__malay_date")}</p>
+                            </div>
+                        </motion.div>
                     </div>
                 </div>
                 <div>
                     <div className="hope_appear">
-                        <div>
-                            <p className='font_written mb_1'>{t("homepage_hope")}</p>
-                        </div>
-                        <div className="rsvp_container">
-                            <div className='font_main small_font font_extra_spacing'>{t("homepage_rsvp")}</div>
-                            <p className='font_main v_small_font font_extra_spacing'>
-                                <a 
-                                    href="https://docs.google.com/forms/d/e/1FAIpQLSeHz_Ge78j_BSThaY3NEYHd7BEi1B8QtIjHvVNSlxrSCLrCBA/viewform?usp=sf_link"
-                                    target="_blank">
-                                    {t("homepage_rsvp_english")}
-                                </a>
-                            </p>
-                            <p className='font_main v_small_font font_extra_spacing'>
-                                <a 
-                                    href="https://docs.google.com/forms/d/e/1FAIpQLSchjZ89KD92DKOF_IXVpQQN9FvQWRfLQg9dXMAKbenU78PoRA/viewform?usp=sf_link"
-                                    target="_blank">
-                                    {t("homepage_rsvp_mandarin")}
-                                </a>
-                            </p>     
-                        </div>
+                        <motion.div 
+                            initial={{ opacity: 0, y: -30, scale: 0.5 }}
+                            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                            transition={{ duration: 0.9 }} 
+                            >
+                            <div>
+                                <p className='font_written mb_1'>{t("homepage_hope")}</p>
+                            </div>
+                            <div className="rsvp_container">
+                                <div className='font_main small_font font_extra_spacing'>{t("homepage_rsvp")}</div>
+                                <p className='font_main v_small_font font_extra_spacing'>
+                                    <a 
+                                        href="https://docs.google.com/forms/d/e/1FAIpQLSeHz_Ge78j_BSThaY3NEYHd7BEi1B8QtIjHvVNSlxrSCLrCBA/viewform?usp=sf_link"
+                                        target="_blank">
+                                        {t("homepage_rsvp_english")}
+                                    </a>
+                                </p>
+                                <p className='font_main v_small_font font_extra_spacing'>
+                                    <a 
+                                        href="https://docs.google.com/forms/d/e/1FAIpQLSchjZ89KD92DKOF_IXVpQQN9FvQWRfLQg9dXMAKbenU78PoRA/viewform?usp=sf_link"
+                                        target="_blank">
+                                        {t("homepage_rsvp_mandarin")}
+                                    </a>
+                                </p>     
+                            </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
             <div className="section paypal_section_container flex_reverse double_height font_light small_font">
                 <div className="box square font_normal">
                 <motion.div 
-                    initial={{ opacity: 0, y: -50, scale: 0.7 }}
+                    initial={{ opacity: 0, y: -30, scale: 0.5 }}
                     whileInView={{ opacity: 1, y: 0, scale: 1 }}
-                    transition={{ duration: 0.5 }} 
+                    transition={{ duration: 0.9 }} 
                 >
                     <p>
                         {t("homepage_paypal_message")}</p>
@@ -127,44 +136,56 @@ export function Homepage() {
                 <div className="box square background_image bristol_background">
                 </div>
                 <div className="box square font_normal">
-                    <div className="day_plan_container">
-                        <h2 className="font_written">{t("uk_day_plan_title")}</h2>
-                        <div className="font_main v_small_font font_extra_spacing">
-                            <h3>{t("uk_day_plan_date")}</h3>
-                            <ul>
-                                <li>
-                                    <p>{t("uk_day_plan_registration")}</p>
-                                    <p>{t("uk_day_plan_registration_location")}</p>
-                                </li>    
-                                <li>
-                                    <p>{t("uk_day_plan_reception")}</p>
-                                    <p>{t("uk_day_plan_reception_location")}</p>
-                                </li>
-                            </ul>
+                    <motion.div 
+                        initial={{ opacity: 0, y: -30, scale: 0.5 }}
+                        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ duration: 0.9 }} 
+                        >
+                        <div className="day_plan_container">
+                            <h2 className="font_written">{t("uk_day_plan_title")}</h2>
+                            <div className="font_main v_small_font font_extra_spacing">
+                                <h3>{t("uk_day_plan_date")}</h3>
+                                <ul>
+                                    <li>
+                                        <p>{t("uk_day_plan_registration")}</p>
+                                        <p>{t("uk_day_plan_registration_location")}</p>
+                                    </li>    
+                                    <li>
+                                        <p>{t("uk_day_plan_reception")}</p>
+                                        <p>{t("uk_day_plan_reception_location")}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <UK_modal />
                         </div>
-                        <UK_modal />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
             <div className="section flex_reverse double_height font_light large_font">
                 <div className="box square font_normal">
-                    <div className="day_plan_container">
-                        <h2 className="font_written">{t("malay_day_plan_title")}</h2>
-                        <div className="font_main v_small_font font_extra_spacing">
-                            <h3>{t("malay_day_plan_date")}</h3>
-                            <ul>
-                                <li>
-                                    <p>{t("malay_day_plan_registration")}</p>
-                                    <p>{t("malay_day_plan_registration_location")}</p>
-                                </li>    
-                                <li>
-                                    <p>{t("malay_day_plan_reception")}</p>
-                                    <p>{t("malay_day_plan_reception_location")}</p>
-                                </li>
-                            </ul>
+                    <motion.div 
+                        initial={{ opacity: 0, y: -30, scale: 0.5 }}
+                        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ duration: 0.9 }} 
+                        >
+                        <div className="day_plan_container">
+                            <h2 className="font_written">{t("malay_day_plan_title")}</h2>
+                            <div className="font_main v_small_font font_extra_spacing">
+                                <h3>{t("malay_day_plan_date")}</h3>
+                                <ul>
+                                    <li>
+                                        <p>{t("malay_day_plan_registration")}</p>
+                                        <p>{t("malay_day_plan_registration_location")}</p>
+                                    </li>    
+                                    <li>
+                                        <p>{t("malay_day_plan_reception")}</p>
+                                        <p>{t("malay_day_plan_reception_location")}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <Malaysia_modal />
                         </div>
-                        <Malaysia_modal />
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="box square background_image kl_background">
                 </div>
