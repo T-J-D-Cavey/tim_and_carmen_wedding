19e4e3cd545d0e9211a15c942ffe19c3bb3143cf
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-11-09 23:33:59 +0000

    fix mistake in file page for UKPage component - this case change may not take affect in github preventing further builds

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { is_correct_selector, set_in_local_storage, in_local_storage_selector } from './redux/main_slice';
 import { Password_modal } from './componants/widgets/Password_modal.tsx';
 import { Homepage } from './pages/Homepage.tsx';
-import { UKPage } from './pages/UKpage.tsx';
+import { UKPage } from './pages/UKPage.tsx';
 import { KLPage } from './pages/KLPage.tsx';
 import { Footer } from './componants/widgets/Footer.tsx';
 import './App.css';
