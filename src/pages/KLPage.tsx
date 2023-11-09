e6be187fc225b6da44eae23db6cd153fa71aadb0
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-11-09 23:05:50 +0000

    write first draft of UK fact sheet

--- src/pages/KLPage.tsx
+++ src/pages/KLPage.tsx
@@ -1,5 +1,60 @@
 export function KLPage() {
-    return (
-        <h1>kl page</h1>
-    )
+  return (
+    <section className="fact-sheet_section_container v_small_font">
+      <div className="fact-sheet_header_container">
+        <h1 className="font_written">Our English Celebration</h1>
+        <h2 className="font_main small_font">What To Expect On The Day</h2>
+      </div>
+      <div>
+        <h3 className="fact-sheet_list_header font_main medium_v_small_font">
+          Bristol Register Office
+        </h3>
+        <ul>
+          <li>Point one</li>
+          <li>Point two</li>
+          <li>Point three</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="fact-sheet_list_header font_main medium_v_small_font">
+          Harbour House
+        </h3>
+        <ul>
+          <li>Point one</li>
+          <li>Point two</li>
+          <li>Point three</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="fact-sheet_list_header font_main medium_v_small_font">
+          What Happens Afterwards
+        </h3>
+        <ul>
+          <li>Point one</li>
+          <li>Point two</li>
+          <li>Point three</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="fact-sheet_list_header font_main medium_v_small_font">
+          Bristol Accommodation
+        </h3>
+        <ul>
+          <li>Point one</li>
+          <li>Point two</li>
+          <li>Point three</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="fact-sheet_list_header font_main medium_v_small_font">
+          Our Bristol Recommendations
+        </h3>
+        <ul>
+          <li>Point one</li>
+          <li>Point two</li>
+          <li>Point three</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
